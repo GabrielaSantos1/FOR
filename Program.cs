3b10814d0ed647ac327bcbf9cbb6038fd8cd505d
GabrielaSantos1 <104846859+GabrielaSantos1@users.noreply.github.com>
2023-06-05 21:41:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Programa
+{
+    static void Main()
+    {
+        int num = 0;
+
+        Console.WriteLine("Digite um número inteiro positivo: ");
+        num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Os primeiros " + num + " números pares são:");
+
+        for (int i = 2; i <= num * 2; i += 2)
+        {
+            Console.WriteLine( i + " ");
+        }
+
+    }
+
+}
